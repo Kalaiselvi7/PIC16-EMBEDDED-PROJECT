ca173c2962df84c95c225b9707f214f1530ab4fc
Kalaiselvi7 <146445418+Kalaiselvi7@users.noreply.github.com>
2025-09-12 16:11:17 +0530

    Add files via upload

---
+++ 04. 7 SEGMENT COMMON ANODE AND COMMON CATHODE/using_CA.c
@@ -0,0 +1,35 @@
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+#define LED PORTB
+
+int main()
+{
+    TRISB = 0x00;
+	int letters[] = {0x88,0x80,0xc6,0xc0,0x86,0x8e,0x8c};
+	while(1)
+	{
+		int i;
+		for(i=0;i<7;i++)
+		{
+			__delay_ms(2000);
+			LED = letters[i];
+		}
+	}
+}
---
+++ 04. 7 SEGMENT COMMON ANODE AND COMMON CATHODE/using_CC.c
@@ -0,0 +1,35 @@
+// PIC16F877A Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR disabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#define _XTAL_FREQ 20000000
+#define LED PORTB
+
+int main()
+{
+    TRISB = 0x00;
+   int numbers[] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0xff,0x67};
+	while(1)
+	{
+		int i;
+		for(i=0;i<10;i++)
+		{
+			__delay_ms(2000);
+			LED = numbers[i];
+		}
+    }
+}
